0036f4d03eed68140effd52d7044ce5065119187
superuser4 <superuser3344@protonmail.com>
2025-04-09 15:16:53 -0400

    restarting:

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-// https://vite.dev/config/
-
-export default defineConfig({
-  base: '/',  // Root directory, no subfolder needed
-});
-
