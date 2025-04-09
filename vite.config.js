f6024967897b2cb2739e8bd91611394fe59dba43
superuser4 <superuser3344@protonmail.com>
2025-04-09 15:09:53 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [svelte()],
-})
+  base: '/superuser4.github.io/',  // Replace with your repository name
+});
+
