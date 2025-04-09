1a120a474d880b1dcf70c4098775d30bc82c256b
superuser4 <superuser3344@protonmail.com>
2025-04-09 16:31:42 -0400

    Updates

--- src/components/DesktopIcon.js
+++
@@ -1,8 +0,0 @@
-export default function DesktopIcon({ icon, label, onDoubleClick }) {
-    return (
-      <div className="desktop-icon" onDoubleClick={onDoubleClick}>
-        <div className="icon-image">{icon}</div>
-        <div className="icon-label">{label}</div>
-      </div>
-    );
-}
