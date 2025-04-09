a20226d543d7447d947704ba78dc6d92b43439f1
superuser4 <superuser3344@protonmail.com>
2025-04-09 15:57:49 -0400

    working?

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  publicPath: '/'
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
